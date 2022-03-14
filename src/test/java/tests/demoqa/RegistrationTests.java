463baf90d1995c6446f3852e607e3182edd63c89
uroborosspowers <z.city17@gmail.com>
2022-03-14 19:45:56 +0300

    Homework 9

--- src/test/java/tests/demoqa/RegistrationTests.java
+++ src/test/java/tests/demoqa/RegistrationTests.java
@@ -11,38 +11,30 @@ public class RegistrationTests extends TestBase {
 
     @Test
     void registrationTest() {
-        open("https://demoqa.com/automation-practice-form");
-
+        open("/automation-practice-form");
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
-        $("#firstName").setValue("Max");
-        $("#lastName").setValue("Zero");
-        $("#userEmail").setValue("ozero@mail.com");
-        $("[for=\"gender-radio-3\"]").click();
+
+        $("#firstName").setValue("Alex");
+        $("#lastName").setValue("Egorov");
+        $("#userEmail").setValue("alex@egorov.com");
+        $("#genterWrapper").$(byText("Other")).click();
+        $("#userNumber").setValue("1231231230");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("July");
         $(".react-datepicker__year-select").selectOption("2008");
-        $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
+        $("[aria-label$='July 30th, 2008']").click();
         $("#subjectsInput").setValue("Math").pressEnter();
-        $("#hobbiesWrapper").$(byText("Music")).click();
-//        $("#uploadPicture").uploadFromClasspath("resources/img/1.png");
-        $("#userNumber").setValue("8800555353");
-        $("#currentAddress").setValue("www.leningrad-spb.ru");
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+//        $("#uploadPicture").uploadFromClasspath("img/1.png");
+        $("#currentAddress").setValue("Some address 1");
         $("#state").scrollTo().click();
         $("#stateCity-wrapper").$(byText("NCR")).click();
         $("#city").click();
         $("#stateCity-wrapper").$(byText("Noida")).click();
         $("#submit").click();
 
-        $(".table").shouldHave(text("Student Name"), text("Max Zero"));
-        $(".table").shouldHave(text("Student Email"), text("ozero@mail.com"));
-        $(".table").shouldHave(text("Gender"), text("Other"));
-        $(".table").shouldHave(text("Mobile"), text("8800555353"));
-        $(".table").shouldHave(text("Date of Birth"), text("30 July,2008"));
-        $(".table").shouldHave(text("Subjects"), text("Maths"));
-        $(".table").shouldHave(text("Hobbies"), text("Music"));
-        $(".table").shouldHave(text("Picture"), text("1.png"));
-        $(".table").shouldHave(text("Address"), text("www.leningrad-spb.ru"));
-        $(".table").shouldHave(text("State and City"), text("NCR Noida"));
-        $("#closeLargeModal").scrollTo().pressEnter();
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").$(byText("Student Name"))
+                .parent().shouldHave(text("Alex Egorov"));
     }
 }
